c3084ff5c3824e7ab2fe0949cddda02195c474e9
Jorge <jcollazo151@west-mec.org>
2021-04-30 14:33:15 -0700

    Attempted to add animation to skills page

--- js/swiper.js
+++ js/swiper.js
@@ -16,14 +16,31 @@
 //     },
 //   });
 
-var swiper = new Swiper('.swiper-container', {
-    effect: 'flip',
-    grabCursor: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+// var swiper = new Swiper('.swiper-container', {
+//     effect: 'flip',
+//     grabCursor: true,
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//   });
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
