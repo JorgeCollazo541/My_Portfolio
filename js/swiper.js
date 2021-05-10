ef73b0e2b79451079b73cdfe7659ad22a38fe334
Jorge <jcollazo151@west-mec.org>
2021-05-10 15:24:43 -0700

    Added an image and tried to make them overlap

--- js/swiper.js
+++ js/swiper.js
@@ -1,38 +1,25 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'flip',
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
 // var swiper = new Swiper('.swiper-container', {
-//     slidesPerView: 4,
-//     spaceBetween: 25,
-//     centeredSlides: true,
-//     autoplay: {
+//   effect: 'coverflow',
+//   grabCursor: true,
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   slidesPerView: 'auto',
+//   autoplay: {
 //       delay: 1500,
 //       disableOnInteraction: false,
-//     },
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//     },
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   });
-
-// var swiper = new Swiper('.swiper-container', {
-//     effect: 'flip',
-//     grabCursor: true,
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   });
-
-// var swiper = new Swiper(".mySwiper", {
-//   effect: "coverflow",
-//   grabCursor: true,
-//   centeredSlides: true,
-//   slidesPerView: "auto",
+//   },
 //   coverflowEffect: {
 //     rotate: 50,
 //     stretch: 0,
@@ -41,23 +28,6 @@
 //     slideShadows: true,
 //   },
 //   pagination: {
-//     el: ".swiper-pagination",
+//     el: '.swiper-pagination',
 //   },
 // });
-
-var swiper = new Swiper('.swiper-container', {
-  effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-  },
-});
